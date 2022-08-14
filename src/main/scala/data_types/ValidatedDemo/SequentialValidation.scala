3f1089a0191414bdf87d1bf62e734292d83da0b7
Nick-M1 <91367903+Nick-M1@users.noreply.github.com>
2022-08-14 08:47:43 +0100

    Delete src/main directory

--- src/main/scala/data_types/ValidatedDemo/SequentialValidation.scala
+++
@@ -1,8 +0,0 @@
-package data_types.ValidatedDemo
-
-/* For when we do want error-accumulation but occasionally run into places where you sequential validation.
-   Sequential Validation = A validation process relies on a previous validation process                     */
-
-object SequentialValidation {
-
-}
